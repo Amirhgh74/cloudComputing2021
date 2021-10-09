8bab2b6e7631d4c5634929f705ffcd00d36f4e65
fmjubori <fmjubori@gmail.com>
2021-10-08 21:26:22 -0700

    Adding drawing feature, M4, and T2 metrics readings

--- Fawaz/test.py
+++ Fawaz/test.py
@@ -5,7 +5,7 @@ from pprint import pprint
 import dateutil.tz
 import dateutil.parser
 from datetime import datetime
-
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -34,38 +34,55 @@ def get_metric_data(configurations):
 
 
 def main():
-    metric_id = []
-    metric_data = []
-    temp = []
-    metric_timestamp = []
+    metric_id_m4 = []
+    metric_data_m4 = []
+    metric_timestamp_m4 = []
+    temp1=[]
+    temp2=[]
+    metric_id_t2 = []
+    metric_data_t2 = []
+    metric_timestamp_t2 = []
+
 
     client = initialize_client()
     
     # Read 1 Metric and print it
-    json_file = json.load(open("fil.json", "r"))
+    json_file1 = json.load(open("m4_cluster.json", "r"))
+
+    # pprint(json_file)
+    response1 = get_metric_data(json_file1)
+    size1 = len (response1['MetricDataResults'])
+
+    for i in range(0, size1):
+        metric_id_m4.append(response1['MetricDataResults'][i]['Id'])
+        metric_data_m4.append(response1['MetricDataResults'][i]['Values'])
+        temp1 = [z.strftime("%H:%M:%S") for z in (response1['MetricDataResults'][i]['Timestamps'])]  
+        metric_timestamp_m4.append(temp1) 
+
+          
+    # Read 1 Metric and print it
+    json_file2 = json.load(open("t2_cluster.json", "r"))
 
     # pprint(json_file)
 
-    response = get_metric_data(json_file)
-    size = len (response['MetricDataResults'])
+    response2 = get_metric_data(json_file2)
+    size2 = len (response2['MetricDataResults'])
 
-    for i in range(0, size):
-        metric_id.append(response['MetricDataResults'][i]['Id'])
-        metric_data.append(response['MetricDataResults'][i]['Values'])
-        temp = [z.strftime("%H:%M:%S") for z in (response['MetricDataResults'][i]['Timestamps'])]  
-        metric_timestamp.append(temp)   
+    for i in range(0, size1):
+        metric_id_t2.append(response2['MetricDataResults'][i]['Id'])
+        metric_data_t2.append(response2['MetricDataResults'][i]['Values'])
+        temp2 = [z.strftime("%H:%M:%S") for z in (response2['MetricDataResults'][i]['Timestamps'])]  
+        metric_timestamp_t2.append(temp2)
+    
 
-        
-
-    print("First Parameter\n")
-    print ("ID = ", metric_id[0])
-    print ("Value = ", metric_data[0])
-    print ("Timestamp = ", metric_timestamp[0])
-    print("\n\n")
-    print("Second Parameter\n")
-    print ("ID = ", metric_id[1])
-    print ("Value = ", metric_data[1])
-    print ("Timestamp = ", metric_timestamp[1])
+    fig, axs = plt.subplots(5, 3)
+    axs = axs.flatten()
+    for i in range(15):
+        axs[i].plot(metric_timestamp_t2[i], metric_data_t2[i], label=metric_id_t2[i])
+        axs[i].plot(metric_timestamp_m4[i], metric_data_m4[i], label=metric_id_m4[i])
+    plt.show()
+    
+    
     return 0
 
 
