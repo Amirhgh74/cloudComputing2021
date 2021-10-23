d404f2596ec281c9ab190605fc70f1a586829e96
fmjubori <fmjubori@gmail.com>
2021-10-22 21:55:28 -0400

    Adding new files for demo

--- metrics.py
+++ metrics.py
@@ -19,7 +19,7 @@ from matplotlib.ticker import MaxNLocator
 def initialize_client():
     client = boto3.client(
         'cloudwatch',
-        region_name='us-east-1'
+        region_name='us-east-1c'
     )
     return client
 
@@ -31,7 +31,7 @@ def get_metric_data(configurations):
 
     # Make GetMetricData API request.
     metric_data = metric_client.get_metric_data(
-        StartTime = (datetime.utcnow() - timedelta(minutes = 30)),
+        StartTime = (datetime.utcnow() - timedelta(hours = 8)),
         EndTime = (datetime.utcnow()),
         **configurations
     )
@@ -66,7 +66,7 @@ def main():
     client = initialize_client()
     
     # Read 1 Metric and print it
-    json_file1 = json.load(open("m4_cluster.json", "r"))
+    json_file1 = json.load(open("ec2_cluster_1.json", "r"))
 
     # pprint(json_file)
     response1 = get_metric_data(json_file1)
@@ -80,7 +80,7 @@ def main():
 
           
     # Read 1 Metric and print it
-    json_file2 = json.load(open("t2_cluster.json", "r"))
+    json_file2 = json.load(open("ec2_cluster_2.json", "r"))
 
     # pprint(json_file)
 
@@ -94,10 +94,10 @@ def main():
         metric_timestamp_t2_elb.append(temp2)
 
     # changes size of the figure itself
-    fig1, ax1 = plt.subplots(2, 2,sharex=False,sharey=False)
-    fig2, ax2 = plt.subplots(2, 2,sharex=False,sharey=False)
-    fig3, ax3 = plt.subplots(2, 2,sharex=False,sharey=False)
-    fig4, ax4 = plt.subplots(2, 2,sharex=False,sharey=False)
+    fig1, ax1 = plt.subplots(2, 2,sharex=False,sharey=False, num="ELB Metrics 1")
+    fig2, ax2 = plt.subplots(2, 2,sharex=False,sharey=False, num="ELB Metrics 2")
+    fig3, ax3 = plt.subplots(2, 2,sharex=False,sharey=False, num="ELB Metrics 3")
+    fig4, ax4 = plt.subplots(2, 2,sharex=False,sharey=False, num="ELB Metrics 4")
     
 
     ax1 = ax1.flatten()
@@ -107,9 +107,9 @@ def main():
 
     i = 0
     while i < 4:
-        ax1[i].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_T2"), s = 5)
+        ax1[i].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_Grp2"), s = 8)
         ax1[i].legend(frameon=True)
-        ax1[i].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_M4"), s = 5)
+        ax1[i].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_Grp1"), s = 8)
         ax1[i].legend(frameon=True)
         plt.setp(ax1[i].get_xticklabels(), rotation=90)
         ax1[i].tick_params(axis="x", labelsize=6)
@@ -122,8 +122,8 @@ def main():
     
     while 4 <= i < 8:
         j = i - 4
-        ax2[j].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_T2"), s = 5)
-        ax2[j].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_M2"), s = 5)
+        ax2[j].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_Grp2"), s = 8)
+        ax2[j].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_Grp1"), s = 8)
         ax2[j].legend(frameon=True)
         plt.setp(ax2[j].get_xticklabels(), rotation=90)
         ax2[j].tick_params(axis="x", labelsize=6)
@@ -133,8 +133,8 @@ def main():
 
     while 8 <= i < 12:
         j = i - 8
-        ax3[j].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_T2"), s = 5)
-        ax3[j].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_M2"), s = 5)
+        ax3[j].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_Grp2"), s = 8)
+        ax3[j].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_Grp1"), s = 8)
         ax3[j].legend(frameon=True)
         plt.setp(ax3[j].get_xticklabels(), rotation=90)
         ax3[j].tick_params(axis="x", labelsize=6)
@@ -145,8 +145,8 @@ def main():
 
     while 12 <= i < 15:
         j = i - 12
-        ax4[j].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_T2"), s = 5)
-        ax4[j].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_M4"), s = 5)
+        ax4[j].scatter(metric_timestamp_t2_elb[i], metric_data_t2_elb[i], label=(str(metric_id_t2_elb[i]) + "_Grp2"), s = 8)
+        ax4[j].scatter(metric_timestamp_m4_elb[i], metric_data_m4_elb[i], label=(str(metric_id_m4_elb[i]) + "_Grp1"), s = 8)
         ax4[j].legend(frameon=True)
         plt.setp(ax4[j].get_xticklabels(), rotation=90)
         ax4[j].tick_params(axis="x", labelsize=6)
@@ -156,7 +156,7 @@ def main():
     #### Get Instance Metrics#####
 
     # Read 1 Metric and print it
-    json_file3 = json.load(open("ec2_m4.json", "r"))
+    json_file3 = json.load(open("ec2_1.json", "r"))
     response3 = get_metric_data(json_file3)
     size3 = len (response3['MetricDataResults'])
 
@@ -167,7 +167,7 @@ def main():
         metric_timestamp_m4_ec2.append(temp3)
     
     # Read 1 Metric and print it
-    json_file4 = json.load(open("ec2_t2.json", "r"))
+    json_file4 = json.load(open("ec2_2.json", "r"))
     response4 = get_metric_data(json_file4)
     size4 = len (response4['MetricDataResults'])
 
@@ -179,10 +179,10 @@ def main():
 
     ##HERE
     # changes size of the figure itself
-    fig5, ax5 = plt.subplots(2, 2,sharex=False,sharey=False)
-    fig6, ax6 = plt.subplots(2, 2,sharex=False,sharey=False)
-    fig7, ax7 = plt.subplots(2, 2,sharex=False,sharey=False)
-    fig8, ax8 = plt.subplots(2, 2,sharex=False,sharey=False)
+    fig5, ax5 = plt.subplots(2, 2,sharex=False,sharey=False, num="EC2 Metrics 1")
+    fig6, ax6 = plt.subplots(2, 2,sharex=False,sharey=False, num="EC2 Metrics 2")
+    fig7, ax7 = plt.subplots(2, 2,sharex=False,sharey=False, num="EC2 Metrics 3")
+    fig8, ax8 = plt.subplots(2, 2,sharex=False,sharey=False, num="EC2 Metrics 3")
 
     ax5 = ax5.flatten()
     ax6 = ax6.flatten()
@@ -193,8 +193,8 @@ def main():
     
     while a < 4:
         for  b in range (a, len(metric_id_t2_ec2), 13):
-            ax5[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 5)
-            ax5[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 5)
+            ax5[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 8)
+            ax5[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 8)
         ax5[a].legend(frameon=True)
         ax5[a].tick_params(axis="x", labelsize=6)
         ax5[a].legend(frameon=True)
@@ -206,8 +206,8 @@ def main():
     a = 0
     while a < 4:
         for  b in range (a+4, len(metric_id_t2_ec2), 13):
-            ax6[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 5)
-            ax6[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 5)
+            ax6[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 8)
+            ax6[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 8)
         ax6[a].legend(frameon=True)
         ax6[a].tick_params(axis="x", labelsize=6)
         ax6[a].legend(frameon=True)
@@ -219,8 +219,8 @@ def main():
     a = 0
     while a < 4:
         for  b in range (a+8, len(metric_id_t2_ec2), 13):
-            ax7[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 5)
-            ax7[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 5)
+            ax7[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 8)
+            ax7[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 8)
         ax7[a].legend(frameon=True)
         ax7[a].tick_params(axis="x", labelsize=6)
         ax7[a].legend(frameon=True)
@@ -231,8 +231,8 @@ def main():
     a = 0
     while a < 4:
         for  b in range (a+12, len(metric_id_t2_ec2), 13):
-            ax8[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 5)
-            ax8[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 5)
+            ax8[a].scatter(metric_timestamp_t2_ec2[b], metric_data_t2_ec2[b], label =str(metric_id_t2_ec2[b]), s = 8)
+            ax8[a].scatter(metric_timestamp_m4_ec2[b], metric_data_m4_ec2[b], label=str(metric_id_m4_ec2[b]), s = 8)
         ax8[a].legend(frameon=True)
         ax8[a].tick_params(axis="x", labelsize=6)
         ax8[a].legend(frameon=True)
