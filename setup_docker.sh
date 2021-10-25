962b359eb3798810ce5003e3c37b7325ec0f7b54
Amir Haghshenas <haghshenas.amir74@gmail.com>
2021-10-25 16:17:20 -0400

    added setup docker script

---
+++ setup_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo docker build -t python_client .
+sudo docker run -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -v ~/.aws/credentials:/root/.aws/credentials:ro \
+    python_client
